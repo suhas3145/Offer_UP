25b222efbf4af73d5d229bba7afa04ab8883a4ad
V Suhas <suhas3145@gmail.com>
2023-05-03 01:19:25 +0530

    Second-commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+
+// export default function Home() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
+
