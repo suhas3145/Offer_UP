4040f5ab547fc3eb91b84103510d481dbe524d7e
V Suhas <suhas3145@gmail.com>
2023-05-04 03:10:35 +0530

    Day2 first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,17 @@
 
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
+import Slider from '../components/Slider'
+
+
 
 const Home = () => {
   return (
     <div>
+      <Announcement/>
       <Navbar/>
+      <Slider/>
     </div>
   )
 }
