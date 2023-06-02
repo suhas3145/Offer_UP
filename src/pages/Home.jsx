e0edd461ed24ec99e94ff6ab02240141a6d9fc3a
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-06-03 02:14:22 +0530

    added new changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,7 @@ import Navbar from '../components/Navbar'
 import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
 import Categories from '../components/Categories'
+import Products from '../components/Products'
 
 
 
@@ -25,6 +26,7 @@ const Home = () => {
       <Navbar/>
       <Slider/>
       <Categories/>
+      <Products/>
 
     </div>
   )
