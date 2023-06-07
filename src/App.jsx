471fe756c2fdab95e0a750c04a4783e081d2b6ca
VinayakSingh2001 <vinayakthakur843@gmail.com>
2023-06-08 00:55:49 +0530

    added new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+
+import Login from "./pages/Login";
 import Product from "./pages/Product";
+import Register from "./pages/Register";
 // import Home from "./pages/Home";
 // import ProductList from "./pages/ProductList";
 
 
 const App = () =>{
-    return <Product/>;
+    return <Login/>;
 };
 
 export default App;
